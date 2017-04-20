e36a5f6b4a75b9653fd922b23324211d1d7331d9
JMF-Arnieri-Hernandez <jmfa89@ufl.edu>
2017-04-19 23:47:36 -0400

    add smoothscroll plugin

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
     $('iframe').addClass('iframe');
 });
 
-$(document).ready(function() {
-    $('img').mouseenter(function() {
-    $(this).closest('img').css('filter', 'grayscale(100%)');
+$(document).ready(function () {
+    $('img').mouseenter(function () {
+        $(this).closest('img').css('filter', 'grayscale(100%)');
         $(this).closest('article').append('<div>You hovered over the image</div>');
     });
 });
