44d3759bd08353b5c86d202db0043fc8d6485fe9
JMF-Arnieri-Hernandez <jmfa89@ufl.edu>
2017-04-19 23:32:38 -0400

    adjust formatting to allow for interactive jQuery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
     $('iframe').addClass('iframe');
 });
+
+$(document).ready(function() {
+    $('img').mouseenter(function() {
+    $(this).closest('img').css('filter', 'grayscale(100%)');
+        $(this).closest('article').append('<div>You hovered over the image</div>');
+    });
+});
